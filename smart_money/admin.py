bbec6a0a4e7f63067116c2e84b2f7e3f56eb7220
Derek Styer <dmstyer@gmail.com>
2016-11-30 16:03:55 -0700

    working db

---
+++ smart_money/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from smart_money.models import Company
+# Register your models here.
+admin.site.register(Company)
---
+++ smart_money/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SmartMoneyConfig(AppConfig):
+    name = 'smart_money'

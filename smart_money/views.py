1b637dc9cffc8a9b8491586920987e9a69f23057
Derek Styer <dmstyer@gmail.com>
2016-11-30 21:03:04 -0700

    template index

--- smart_money/views.py
+++ smart_money/views.py
@@ -4,4 +4,7 @@ from django.template import Context, loader
 from smart_money.models import Company
 # Create your views here.
 def index(request):
-    return HttpResponse(loader.get_template('smart_money/index.html'))
+    companies_byname= Company.objects.all().order_by('-name')
+    x = loader.get_template('smart_money/index.html')
+    c = Context({'companies_byname':companies_byname, })
+    return HttpResponse(x.render())
--- smartmoney_project/urls.py
+++ smartmoney_project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^company/', include('smart_money.urls'))
+    url(r'^company/', include('smart_money.urls')),
 ]

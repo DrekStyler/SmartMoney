e39f614d351eefe3b612391e8d7a94ddb5393689
Derek Styer <dmstyer@gmail.com>
2016-11-30 20:43:51 -0700

    templates working

--- smart_money/views.py
+++ smart_money/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import Context, loader
+from smart_money.models import Company
 # Create your views here.
+def index(request):
+    return HttpResponse(loader.get_template('smart_money/index.html'))
